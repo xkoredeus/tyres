2660c23e875e7012a1c9a953c91a7a14590fe2ea
xkoredeus <xkored@gmail.com>
2020-02-17 15:30:27 +0400

    feat: adapt

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -7,59 +7,41 @@
     // }, 4000);
 // });
 $(() => {
-   // $('.banner__descr-link').on('click', function () {
-   //      $('html, body').animate({
-   //          scrollTop: $("#profit").offset().top - 50
-   //      }, 700);
-   //  });
-   //  $('.js-popup__open').on('click', function () {
-   //      $('body').addClass('menu-open');
-   //  });
-   //  $('.js-popup__close').on('click', function () {
-   //      $('body').removeClass('menu-open');
-   //  });
-   //  $('.js-banner__overlay').on('click', function () {
-   //      $('body').removeClass('menu-open');
-   //  });
-
+  if ( $(window).width() > 1285 ) {
+    $('.banner__btn').on('click', function () {
+      $('.aside__btn').trigger('click');
+    });
+  }
+});
+// $(() => {
     //Закрываем AjaxForm popup после успешной отправки
     // $(document).on('af_complete', function(event,res) {
     //   if(res.success) parent.$.fancybox.close();
     // });
-});
+// });
 $(() => {
-    // let tl0   = new TimelineMax()
-    //     tl1p1 = new TimelineMax()
-    //     tl1p2 = new TimelineMax();
-    // const controller = new ScrollMagic.Controller();
-    // tl0
-    //     .fromTo('.preloader__logo', .4, {x:10,opacity:0},{x:0,opacity:1}, 0.5)
-    //     .staggerFromTo('.preloader__title', .3, {x:10,opacity:0},{x:0,opacity:1}, 0.3)
-    //     .staggerFromTo('.preloader__subtitle', .3, {x:10,opacity:0},{x:0,opacity:1}, 0.3)
-    //     .staggerFromTo('.preloader', 1, {scale:1}, {scale:1.1,ease: Linear.easeNone,repeatDelay:0, repeat:-1, yoyo:true}, 0)
-    // tl1p1
-    //     .fromTo('.banner__bg', 1.5, {scale:1.05,opacity:0}, {scale:1,opacity:1}, "+=3")
-    //     .staggerFromTo('.banner__subttl', .5, {x:-30,opacity:0}, {x:0,opacity:1}, 0.3)
-    //     .staggerFromTo('.banner__ttl', .5, {x:-30,opacity:0}, {x:0,opacity:1}, 0.7)
-    //     .staggerFromTo('.banner__descr', .5, {y:30,opacity:0}, {y:0,opacity:1}, 0.7)
-    //     .staggerFromTo('.header__logo', .2, {x:-30,opacity:0}, {x:0,opacity:1}, 0.7)
-    //     .staggerFromTo('.header__nav-link', .2, {x:30,opacity:0}, {x:0,opacity: 1}, 0.1)
-    //     .staggerFromTo('.header__login-btn', .2, {x:30,opacity:0}, {x:0,opacity:1}, 0.7)
-    //     .staggerFromTo('.header__btn', .2, {scale:0.5,opacity:0}, {scale:1,opacity:1}, 0.7)
-    // tl1p2
-    //     .fromTo('.banner__bg', .7, {y: 0,scale:1}, {y: 20,scale:1.05})
-    //     .fromTo('.banner__in', .5, {y:0}, {y:-30}, "-=0.7")
-    // const scene1p1 = new ScrollMagic.Scene({
-    //     triggerElement: ".banner",
-    //     triggerHook: 0.1,
-    //     reverse: false,
-    // })
-    //     .setTween(tl1p1)
-    //     .addTo(controller);
-    // const scene1p2 = new ScrollMagic.Scene({
-    //     triggerElement: ".banner__descr",
-    //     triggerHook: 0.4,
-    // });
+    let tl1   = new TimelineMax();
+        tl1
+            .fromTo('.aside__btn-arrow', .3, {rotation:0}, {rotation:-90})
+            .staggerFromTo('.aside__btn', .3, {borderRadius: "36px 0 0 36px"}, {borderRadius: "50%", x:-10}, 0.3);
+    const controller = new ScrollMagic.Controller();
+    const scene1p1 = new ScrollMagic.Scene({
+        triggerElement: ".section",
+        triggerHook: 0.8,
+    })
+        .setTween(tl1)
+        .addTo(controller);
+    $('.aside__btn').on('click',function(){
+        $('.content').animate({
+            scrollTop: $("#banner").offset().top - 100
+        }, 500);
+        TweenMax.to('.banner__bg--1', 1, {y:200,opacity:.7});
+        TweenMax.to('.banner__bg--2', 1, {y:0});
+        TweenMax.to('.banner__top', .6, { y:-150, delay:.2 });
+        TweenMax.to('.banner__btn', .2, { opacity:0 });
+        TweenMax.to('.filter', 1, {opacity:1,zIndex:3,delay:1.2});
+        TweenMax.to('.filter__item', .9, {y:1,delay:1.2});
+    });
 });
 $(() => {
     $(".js-select").select2({
@@ -67,7 +49,6 @@ $(() => {
         minimumResultsForSearch: -1
     });
 });
-
 $(() => {
   $('.card__list').owlCarousel({
     nav: true,
@@ -93,11 +74,10 @@ $(() => {
     },
   });
 });
-
 $(() => {
   $('.service__slider').owlCarousel({
     nav: true,
-    loop: false,
+    loop: true,
     smartSpeed: 800,
     dots: false,
     items: 1,
@@ -107,7 +87,6 @@ $(() => {
     navText: ["<svg width='43' height='8' viewBox='0 0 43 8' xmlns='http://www.w3.org/2000/svg'><path d='M43 4L1.5 4M1.5 4L4.5 7M1.5 4L4.5 1'/></svg>","<svg width='43' height='8' viewBox='0 0 43 8' xmlns='http://www.w3.org/2000/svg'><path d='M2.62268e-07 4L41.5 4M41.5 4L38.5 7M41.5 4L38.5 1'/></svg>"],
   });
 });
-
 $(() => {
   $('.js-popup__open').on('click', function () {
     $('.grid-layout').addClass('menu-open');
@@ -119,7 +98,6 @@ $(() => {
     $('.grid-layout').removeClass('menu-open');
   });
 });
-
 $(() => {
   var myMap;
 
@@ -157,4 +135,51 @@ $(() => {
         myMap.geoObjects.add(obj);
     });
   };
+});
+$(() => {
+    $.fn.select2.amd.define('select2/i18n/ru',[],function () {
+        // Russian
+        return {
+            errorLoading: function () {
+                return 'Результат не может быть загружен.';
+            },
+            inputTooLong: function (args) {
+                var overChars = args.input.length - args.maximum;
+                var message = 'Пожалуйста, удалите ' + overChars + ' символ';
+                if (overChars >= 2 && overChars <= 4) {
+                    message += 'а';
+                } else if (overChars >= 5) {
+                    message += 'ов';
+                }
+                return message;
+            },
+            inputTooShort: function (args) {
+                var remainingChars = args.minimum - args.input.length;
+
+                var message = 'Пожалуйста, введите ' + remainingChars + ' или более символов';
+
+                return message;
+            },
+            loadingMore: function () {
+                return 'Загружаем ещё ресурсы…';
+            },
+            maximumSelected: function (args) {
+                var message = 'Вы можете выбрать ' + args.maximum + ' элемент';
+
+                if (args.maximum  >= 2 && args.maximum <= 4) {
+                    message += 'а';
+                } else if (args.maximum >= 5) {
+                    message += 'ов';
+                }
+
+                return message;
+            },
+            noResults: function () {
+                return 'Ничего не найдено';
+            },
+            searching: function () {
+                return 'Поиск…';
+            }
+        };
+    });
 });
